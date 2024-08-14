669a5dc3b1edf1283bd72ff65187160b98b7ed79
Kidus-Nesibu <Knesibu3@gmail.com>
2024-08-15 00:23:29 +0300

    Version 1 task 3

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Base model script """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Class that is inherited from"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return stirng Representation of the class"""
+        return "[{}] ({}) {}" \
+            .format(type(self.__name__), self.id, self.__dict__)
+
+    def to_dict(self):
+        """A dictionary representation of the class"""
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = type(self).__name__
+        dictionary["created_at"] = self.__dict__["created_at"].isoformat()
+        dictionary["updated_at"] = self.__dict__["updated_at"].isoformat()
