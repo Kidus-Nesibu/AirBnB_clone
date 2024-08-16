dc8115a57b479b4d9ce184ea834cf01f00b74425
Kidus-Nesibu <Knesibu3@gmail.com>
2024-08-16 11:43:54 +0300

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,17 @@ from datetime import datetime
 
 class BaseModel:
     """Class that is inherited from"""
+
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return stirng Representation of the class"""
+        """Return stirng presentation of the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def to_dict(self):
@@ -25,16 +26,3 @@ class BaseModel:
         dictionary["created_at"] = self.__dict__["created_at"].isoformat()
         dictionary["updated_at"] = self.__dict__["updated_at"].isoformat()
         return dictionary
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
