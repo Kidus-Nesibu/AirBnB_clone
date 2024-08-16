0eecc0db24a59ebcbfab3821bb7469b4e3de47d9
Kidus-Nesibu <Knesibu3@gmail.com>
2024-08-16 12:35:45 +0300

    task 4 Version 1

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,23 @@ from datetime import datetime
 class BaseModel:
     """Class that is inherited from"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not none and kwargs != {}:
+            for key, value in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                elif key == "updated_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
