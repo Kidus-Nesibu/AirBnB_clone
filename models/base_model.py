5aa20d82a54772e626486a268e4624009d7e42a9
Kidus-Nesibu <Knesibu3@gmail.com>
2024-09-01 17:38:11 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,15 @@
 """This script is the base model"""
 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """Base model that every thing will inherit from"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
