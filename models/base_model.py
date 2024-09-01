72f4c5fea17c5039c802d58830c064c5f0d99ffe
Kidus-Nesibu <Knesibu3@gmail.com>
2024-09-01 17:24:21 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+"""This script is the base model"""
+
 import uuid
 import datetime
+
 
 class BaseModel:
     """Base model that every thing will inherit from"""
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """saves the time that it is created"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         """Converts the methods into dictionary"""
         dictionary = self.__dict__.copy()
         dictionary['class'] = type(self).__name__
         dictionary['created_at'] = datetime.now().isoformat()
         dictionary['updated_at'] = datetime.now().isoformat()
+
         return dictionary
