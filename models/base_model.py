bf9ed6995922f6110a18081c2162b61ecfccb5a1
Kidus-Nesibu <Knesibu3@gmail.com>
2024-09-01 18:55:46 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
