a17f179db840adbbb83176c0c9c76eb93f82b020
Kidus-Nesibu <Knesibu3@gmail.com>
2024-09-06 16:16:04 +0300

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
-                    storage.new()
+                    storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -32,7 +32,7 @@ class BaseModel:
     def save(self):
         """saves the time that it is created"""
         self.updated_at = datetime.now()
-        storage.save()
+        storage.save(self)
 
     def to_dict(self):
         """Converts the methods into dictionary"""
